258edec4d4171d7ae14469befecc23b36c71ac7a
Hnem <149763377+Hnem12@users.noreply.github.com>
2025-03-27 02:44:20 +0700

    Update checkAuthentication.js

--- app/middleware/checkAuthentication.js
+++ app/middleware/checkAuthentication.js
@@ -12,38 +12,38 @@ const rolePermissions = {
 
 // Middleware kiểm tra xác thực và phân quyền
 const checkAuthentication = (req, res, next) => {
-    const token = req.cookies.token; // Lấy token từ cookies
+    // const token = req.cookies.token; // Lấy token từ cookies
 
-    if (!token) {
-        return res.redirect('/'); // Nếu không có token, chuyển hướng về trang đăng nhập
-    }
+    // if (!token) {
+    //     return res.redirect('/'); // Nếu không có token, chuyển hướng về trang đăng nhập
+    // }
 
-    try {
-        const decoded = jwt.verify(token, 'Hnem'); // Giải mã token
-        req.user = decoded; // Gán thông tin người dùng vào request
-        const { role } = req.user; // Lấy vai trò của người dùng
+    // try {
+    //     const decoded = jwt.verify(token, 'Hnem'); // Giải mã token
+    //     req.user = decoded; // Gán thông tin người dùng vào request
+    //     const { role } = req.user; // Lấy vai trò của người dùng
 
-        // Nếu là User, chuyển hướng về trang localhost:5000
-        if (role === 'User') {
-            return res.redirect(api.baseURLFE);
-        }
+    //     // Nếu là User, chuyển hướng về trang localhost:5000
+    //     if (role === 'User') {
+    //         return res.redirect(api.baseURLFE);
+    //     }
 
-        // Kiểm tra quyền dựa trên danh sách rolePermissions
-        const allowedPaths = rolePermissions[role] || [];
+    //     // Kiểm tra quyền dựa trên danh sách rolePermissions
+    //     const allowedPaths = rolePermissions[role] || [];
         
-        // Truyền allowedPaths vào res.locals để sử dụng trong EJS
-        res.locals.userRole = role;
-        res.locals.allowedPaths = allowedPaths;
+    //     // Truyền allowedPaths vào res.locals để sử dụng trong EJS
+    //     res.locals.userRole = role;
+    //     res.locals.allowedPaths = allowedPaths;
 
-        if (allowedPaths.includes(req.path)) {
-            return next(); // Nếu có quyền, tiếp tục xử lý request
-        }
+    //     if (allowedPaths.includes(req.path)) {
+    //         return next(); // Nếu có quyền, tiếp tục xử lý request
+    //     }
 
-        // Nếu không có quyền truy cập, chuyển hướng về trang chủ
-        return res.redirect('/trangchu');
-    } catch (err) {
-        return res.redirect('/api/account/login'); // Nếu token không hợp lệ, chuyển về trang đăng nhập
-    }
+    //     // Nếu không có quyền truy cập, chuyển hướng về trang chủ
+    //     return res.redirect('/trangchu');
+    // } catch (err) {
+    //     return res.redirect('/api/account/login'); // Nếu token không hợp lệ, chuyển về trang đăng nhập
+    // }
 };
 
 module.exports = checkAuthentication;
